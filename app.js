34d6685c3da0c653790075994b4a40a166d833fa
AlexWalker777 <Aleksey_Sergeev777@mail.ru>
2026-04-03 18:25:48 +0300

    правки в апи

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const idInstanceInput = document.getElementById('idInstance');
 const apiTokenInput = document.getElementById('apiToken');
 const apiResponse = document.getElementById('apiResponse');
 
+
 const apiUrl = (method) =>
-  `https://api.green-api.com/waInstance${idInstanceInput.value}/${method}/${apiTokenInput.value}`;
+  `https://4100.api.green-api.com/waInstance${idInstanceInput.value}/${method}/${apiTokenInput.value}`;
+
 
 async function callApi(method, data = {}) {
   try {
-    const response = await fetch(apiUrl(method), {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    });
+    let options = {};
+    // Для getSettings и getStateInstance используем GET, для остальных POST
+    if (method === 'getSettings' || method === 'getStateInstance') {
+      options.method = 'GET';
+    } else {
+      options.method = 'POST';
+      options.headers = { 'Content-Type': 'application/json' };
+      options.body = JSON.stringify(data);
+    }
+    const response = await fetch(apiUrl(method), options);
     const result = await response.json();
     apiResponse.value = JSON.stringify(result, null, 2);
   } catch (error) {
